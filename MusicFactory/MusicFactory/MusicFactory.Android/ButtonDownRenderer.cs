cf1055e3b3bfda9c2bd35ffe0bfa3cdaab66c02f
DESKTOP-RAFNJ9I\My Arcanum <ja.mishaka@gmail.com>
2019-03-26 13:16:00 +0200

    Added button renderer for android project. Issue is fixed

---
+++ MusicFactory/MusicFactory/MusicFactory.Android/ButtonDownRenderer.cs
@@ -0,0 +1,32 @@
+﻿using Android.Views;
+using MusicFactory.Droid;
+using MusicFactory.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Button), typeof(ButtonDownRenderer))]
+namespace MusicFactory.Droid
+{
+    public class ButtonDownRenderer : ButtonRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
+
+            var customButton = e.NewElement as ButtonDown;
+
+            var thisButton = Control as Android.Widget.Button;
+            thisButton.Touch += (object sender, TouchEventArgs args) =>
+            {
+                if (args.Event.Action == MotionEventActions.Down)
+                {
+                    customButton.OnPressed();
+                }
+                else if (args.Event.Action == MotionEventActions.Up)
+                {
+                    customButton.OnReleased();
+                }
+            };
+        }
+    }
+}
